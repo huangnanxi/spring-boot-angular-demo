340239842de3058009d0c9db61bf3bc45f023386
huangnx <hnx_seu@163.com>
2018-08-26 09:25:45 +0800

    controller

---
+++ demo-portal/src/main/java/com/transwarp/demo/portal/form/LoginForm.java
@@ -0,0 +1,7 @@
+package com.transwarp.demo.portal.form;
+
+/**
+ * Created by huangnx on 2018/8/26.
+ */
+public class LoginForm {
+}
---
+++ demo-portal/src/main/java/com/transwarp/demo/portal/form/RegisterForm.java
@@ -0,0 +1,46 @@
+package com.transwarp.demo.portal.controller;
+
+/**
+ * Created by huangnx on 2018/8/26.
+ */
+public class RegisterForm {
+    private String  userName;
+
+    private String  psw;
+
+    private String  realName;
+
+    private Integer age;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPsw() {
+        return psw;
+    }
+
+    public void setPsw(String psw) {
+        this.psw = psw;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
