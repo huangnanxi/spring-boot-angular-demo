9eed25a62f619d7c99675c093b098c6169a0850e
huangnx <hnx_seu@163.com>
2018-08-26 11:49:12 +0800

    controller register and logback

---
+++ demo-common/src/main/java/com/transwarp/demo/common/entity/AppException.java
@@ -0,0 +1,7 @@
+package com.transwarp.demo.common.entity;
+
+/**
+ * Created by huangnx on 2018/8/26.
+ */
+public class AppException {
+}
---
+++ demo-common/src/main/java/com/transwarp/demo/common/trace/TraceLog.java
@@ -0,0 +1,7 @@
+package com.transwarp.demo.common.trace;
+
+/**
+ * Created by huangnx on 2018/8/26.
+ */
+public class TraceLog {
+}
